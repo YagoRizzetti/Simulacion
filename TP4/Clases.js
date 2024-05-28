80c9c1e7c24feb9f9b0f1f0ca9c84a3034db7078
Yago Rizzetti <yagorizzetti15@gmail.com>
2024-05-28 16:53:36 -0300

    creando todas las clases

--- TP4/Clases.js
+++ TP4/Clases.js
@@ -26,36 +26,87 @@ export class Evento{
 }
 
 export class LlegadaCliente extends Evento{
-    constructor(demora, llegada){
+    constructor(random, demora, llegada){
         super(random, demora);
         this.llegada = llegada;
     }
 }
 
 export class AsignacionPeluquero extends Evento{
-    constructor(peluquero){
+    constructor(random, peluquero){
         super(random);
         this.peluquero = peluquero;
     }
 }
 
 export class FinAtencionAprendiz extends Evento{
-    constructor(demora, finAtencion){
+    constructor(random, demora, finAtencion){
         super(random, demora);
         this.finAtencion = finAtencion;
     }
 }
 
 export class FinAtencionVeteranoA extends Evento{
-    constructor(demora, finAtencion){
+    constructor(random, demora, finAtencion){
         super(random, demora);
         this.finAtencion = finAtencion;
     }
 }
 
 export class FinAtencionVeteranoB extends Evento{
-    constructor(demora, finAtencion){
+    constructor(random, demora, finAtencion){
         super(random, demora);
         this.finAtencion = finAtencion;
+    }
+}
+
+class ObjetosPermanentes{
+    constructor(estado, cola, clientesAtendidos){
+        this.estado = estado;
+        this.cola = cola;
+        this.clientesAtendidos = clientesAtendidos;
+    }
+}
+
+export class Aprendiz extends ObjetosPermanentes{
+    constructor(estado, cola, clientesAtendidos){
+        super (estado, cola, clientesAtendidos)
+    }
+}
+
+export class VeteranoA extends ObjetosPermanentes{
+    constructor(estado, cola, clientesAtendidos){
+        super (estado, cola, clientesAtendidos)
+    }
+}
+
+export class VeteranoB extends ObjetosPermanentes{
+    constructor(estado, cola, clientesAtendidos){
+        super (estado, cola, clientesAtendidos)
+    }
+}
+
+export class Recaudacion{
+    constructor(ganancias, gastos, gananciasNetas, promRecaudacion){
+        this.ganancias = ganancias;
+        this.gastos = gastos;
+        this.gananciasNetas = gananciasNetas;
+        this.promedioRecaudacion = promRecaudacion;
+    }
+}
+
+export class Esperas{
+    constructor(esperaSimultaneas, maxEsperaSimultanea){
+        this.esperaSimultanea = esperaSimultaneas;
+        this.maxEsperaSimultanea = maxEsperaSimultanea;
+    }
+}
+
+export class Cliente{
+    constructor(estado, peluquero, momentoRefresco, refresco){
+        this.estado = estado;
+        this.peluquero = peluquero;
+        this.momentoRefresco = momentoRefresco;
+        this.refresco = refresco;
     }
 }
